3f008b570a390095b9fd6c5a7d3d6225f0dcfe95
Kirk <kirkrobertsnyder@gmail.com>
2021-01-19 14:39:20 -0500

    grade counter

--- index.js
+++ index.js
@@ -274,8 +274,22 @@ Using the grade function below do the following:
    below should return 'you got an F'
 */
   
-function grade(/*Your Code here */){
-  /*Your Code here */
+function grade(score){
+    if(score > 89) {
+      return("you got an A");
+    }
+    else if(score > 79) {
+      return("you got a B");
+    }
+    else if (score > 69) {
+      return ("you got a C");
+    }
+    else if (score > 59) {
+      return ("you got a D");
+    }
+    else {
+      return ("you got an F");
+    }
   }
   
   
